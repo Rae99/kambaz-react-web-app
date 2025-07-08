30918eb2e67922711f39eb100788b467a8a6eec0
Rae99 <ruibantian812@gmail.com>
2025-07-07 17:37:30 -0700

    first commit

---
+++ src/Kambaz/Dashboard.tsx
@@ -0,0 +1,116 @@
+import { Link } from "react-router-dom";
+export default function Dashboard() {
+  return (
+    <div id="wd-dashboard">
+      <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
+      <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
+      <div id="wd-dashboard-courses">
+        <div className="wd-dashboard-course">
+          <Link
+            to="/Kambaz/Courses/1234/Home"
+            className="wd-dashboard-course-link"
+          >
+            <img src="/images/reactjs.jpg" width={200} />
+            <div>
+              <h5> CS1234 React JS </h5>
+              <p className="wd-dashboard-course-title">
+                Full Stack software developer{" "}
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+        <div className="wd-dashboard-course">
+          <Link
+            to="/Kambaz/Courses/5678/Home"
+            className="wd-dashboard-course-link"
+          >
+            <img src="/images/expressjs.jpg" width={200} />
+            <div>
+              <h5> CS5678 Express JS </h5>
+              <p className="wd-dashboard-course-title">
+                Full Stack software developer{" "}
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+        <div className="wd-dashboard-course">
+          <Link
+            to="/Kambaz/Courses/91011/Home"
+            className="wd-dashboard-course-link"
+          >
+            <img src="/images/nodejs.jpg" width={200} />
+            <div>
+              <h5> CS91011 Node JS </h5>
+              <p className="wd-dashboard-course-title">
+                Full Stack software developer{" "}
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+        <div className="wd-dashboard-course">
+          <Link
+            to="/Kambaz/Courses/121314/Home"
+            className="wd-dashboard-course-link"
+          >
+            <img src="/images/mongodb.jpg" width={200} />
+            <div>
+              <h5> CS121314 Mongo DB </h5>
+              <p className="wd-dashboard-course-title">
+                Full Stack software developer{" "}
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+        <div className="wd-dashboard-course">
+          <Link
+            to="/Kambaz/Courses/5004/Home"
+            className="wd-dashboard-course-link"
+          >
+            <img src="/images/Java Object-oriented design.jpg" width={200} />
+            <div>
+              <h5> CS5004 Object-oriented design </h5>
+              <p className="wd-dashboard-course-title">
+                Full Stack software developer{" "}
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+        <div className="wd-dashboard-course">
+          <Link
+            to="/Kambaz/Courses/5800/Home"
+            className="wd-dashboard-course-link"
+          >
+            <img src="/images/algorithms.jpg" width={200} />
+            <div>
+              <h5> CS5800 Algorithms </h5>
+              <p className="wd-dashboard-course-title">
+                Full Stack software developer{" "}
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+        <div className="wd-dashboard-course">
+          <Link
+            to="/Kambaz/Courses/5801/Home"
+            className="wd-dashboard-course-link"
+          >
+            <img src="/images/operating systems.jpg" width={200} />
+            <div>
+              <h5> CS5801 Operating Systems </h5>
+              <p className="wd-dashboard-course-title">
+                Full Stack software developer{" "}
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
