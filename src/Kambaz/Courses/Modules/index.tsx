5caf8082e2c5795c778eab8d538b90e67b78cc99
Rae99 <ruibantian812@gmail.com>
2025-07-14 13:41:58 -0700

    fixed module

--- src/Kambaz/Courses/Modules/index.tsx
+++ src/Kambaz/Courses/Modules/index.tsx
@@ -33,12 +33,28 @@ export default function Modules() {
               Learn what is Web Development
               <LessonControlButtons />
             </ListGroup.Item>
+            <ListGroup.Item className="wd-lesson p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" />
+              Lesson 1
+              <LessonControlButtons />
+            </ListGroup.Item>
+            <ListGroup.Item className="wd-lesson p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" />
+              Lesson 2
+              <LessonControlButtons />
+            </ListGroup.Item>
           </ListGroup>
         </ListGroup.Item>
         <ListGroup.Item className="wd-module p-0 mb-5 fs-5 border-gray">
-          <BsGripVertical className="me-2 fs-3" /> Week 2{" "}
-          <ModuleControlButtons />
+          <div className="wd-title p-3 ps-2 bg-secondary">
+            <BsGripVertical className="me-2 fs-3" /> Week 2
+            <ModuleControlButtons />
+          </div>
           <ListGroup className="wd-lessons rounded-0">
+            <ListGroup.Item className="wd-lesson p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" />
+              LEARNING OBJECTIVES <LessonControlButtons />
+            </ListGroup.Item>
             <ListGroup.Item className="wd-lesson p-3 ps-1">
               <BsGripVertical className="me-2 fs-3" />
               LESSON 1 <LessonControlButtons />
