6a2629eb9e0f3e079bd2d2213c3d0b3f45b89d34
Rae99 <ruibantian812@gmail.com>
2025-07-14 17:55:39 -0700

    change Github link

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -29,7 +29,9 @@ export default function TOC() {
         </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="https://github.com/jannunzi">My GitHub</Nav.Link>
+        <Nav.Link href="https://github.com/Rae99/kambaz-react-web-app">
+          My GitHub
+        </Nav.Link>
       </Nav.Item>
     </Nav>
   );
