058e4ac4cc80b2b9daa0c1c0ef08af900a9f28d8
Rae99 <ruibantian812@gmail.com>
2025-08-04 11:40:31 +0800

    5.3.2.4 completed

--- src/Kambaz/Account/client.ts
+++ src/Kambaz/Account/client.ts
@@ -10,5 +10,17 @@ export const signin = async (credentials: any) => {
 export const signup = async (user: any) => {
     const response = await axios.post(`${USERS_API}/signup`, user);
     return response.data;
-  };
+};
   
+export const updateUser = async (user: any) => {
+   const response = await axios.put(`${USERS_API}/${user._id}`, user);
+   return response.data;
+};
+
+export const profile = async () => {
+  const response = await axios.post(`${USERS_API}/profile`);
+  return response.data;
+};
+
+
+
