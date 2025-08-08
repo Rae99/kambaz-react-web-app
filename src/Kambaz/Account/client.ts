127155733eeef7e16cfcee943e8f75f155465420
Rae99 <ruibantian812@gmail.com>
2025-08-08 17:23:50 +0800

    6.2.6.3

--- src/Kambaz/Account/client.ts
+++ src/Kambaz/Account/client.ts
@@ -8,6 +8,16 @@ export const findAllUsers = async () => {
   return response.data;
 };
 
+export const findUsersByRole = async (role: string) => {
+  const response = await axiosWithCredentials.get(`${USERS_API}?role=${role}`);
+  return response.data;
+};
+
+export const findUsersByPartialName = async (name: string) => {
+  const response = await axios.get(`${USERS_API}?name=${name}`);
+  return response.data;
+};
+
 
 export const createCourse = async (course: any) => {
   const { data } = await axiosWithCredentials.post(`${USERS_API}/current/courses`, course);
